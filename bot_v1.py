1d76371073e688c55cd037d0a7f6617fa93f7487
IlyaMirror <86870342+IlyaMirror@users.noreply.github.com>
2022-07-13 17:13:03 +0300

    Dev_ver_0.2

--- bot_v1.py
+++ bot_v1.py
@@ -6,9 +6,11 @@ import random
 
 bot = telebot.TeleBot('5511739630:AAH4XYAW3tuxcPpy3URJLir3o804G_GdvDY')
 movies = pd.read_excel('Movies_1.xlsx')
+series = pd.read_excel('Series_1.xlsx')
+
 
 npdata_m = np.array(movies)
-
+npdata_s = np.array(series)
 
 def rand_movies():
     r = random.randint(0, 9)
@@ -17,6 +19,16 @@ def rand_movies():
     rating = (npdata_m[r] [2:3])
     description = (npdata_m[r] [4:5])
     return name, year, rating, description
+
+
+def rand_series():
+    r = random.randint(0, 8)
+    name = (npdata_s[r] [:1])
+    year = (npdata_s[r] [1:2])
+    rating = (npdata_s[r] [2:3])
+    description = (npdata_s[r] [4:5])
+    return name, year, rating, description
+
 
 
 @bot.message_handler(commands=['start'])
@@ -44,6 +56,14 @@ def bot_message(message):
                               + '\n' + 'Рейтинг: ' + str(*rating_m)
                               + '\n' + '\n' + 'Синопсис: '
                               + str(*descrip_m)))
+        elif message.text == '🎞 Сериал':
+            name_s, year_s, rating_s, descrip_s = rand_series()
+            bot.send_message(message.chat.id,
+                             ('Название: ' + str(*name_s)
+                              + '\n' + 'Год: ' + str(*year_s)
+                              + '\n' + 'Рейтинг: ' + str(*rating_s)
+                              + '\n' + '\n' + 'Синопсис: '
+                              + str(*descrip_s)))
 
 
 
